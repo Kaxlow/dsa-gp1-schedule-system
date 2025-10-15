73ab994088b074601ab6d0b0e9f7ea77f337c12c
Kaxlow <53386415+Kaxlow@users.noreply.github.com>
2025-10-15 00:01:51 -0600

    Delete old src/py_classes_ken directory

--- src/py_classes_ken/utils.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-# Define functions to convert campus event's date and time strings to date-time objects for comparison
-def get_event_dt_start(event):
-  return datetime.strptime(f"{event["date"]} {event["time_start"]}", "%Y-%m-%d %H:%M")
-
-def get_event_dt_end(event):
-  return datetime.strptime(f"{event["date"]} {event["time_end"]}", "%Y-%m-%d %H:%M")
